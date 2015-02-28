a56c872b5c2ca6e2a7922d5c8c1e4af0c024d39c
zgleicher <zachgleicher@gmail.com>
2015-02-28 15:05:55 -0500

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('levelsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
