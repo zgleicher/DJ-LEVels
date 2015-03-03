fb949327d3d304ab6a53c71bfa3a2fac5fb036df
Kristie Howard <kbh2120@columbia.edu>
2015-03-03 00:55:51 -0500

    feat(/landing): nested ui-view is working. need to separate controller.

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -7,14 +7,20 @@ angular.module('levelsApp')
         url: '/landing',
         templateUrl: 'app/landing/landing.html',
         controller: 'LandingCtrl'
-      });
-      // .state('landing.center', {
-      //   views: {
-      //     'center': {
-      //       templateUrl: 'app/landing/landing.center.html',
-      //       controller: function($scope){ $scope.message = 'hello world.'}
-      //     }
-      //   }
-      // })
-  //;
+      })
+      .state('landing.center', {
+        url: '/center/:groupid',
+        views: {
+          'center-content': {
+            templateUrl: 'app/landing/landing.center.html',
+            controller: function($scope, $stateParams){
+              //controller for the center part, ened to extract
+              console.log($stateParams);
+              $scope.message = $stateParams.groupid; 
+              $scope.message2 = 'hello world. this is the UI-center view';
+            }
+          }
+        }
+      })
+  ;
   });
