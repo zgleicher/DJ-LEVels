7a7855a0ae4b4198b2ed6a8f186e5f0c4eee9a72
zgleicher <zachgleicher@gmail.com>
2015-03-13 12:08:56 -0400

    feat(soundcloud): get token from sc. Need to send back to front end and query db

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -31,5 +31,7 @@ angular.module('levelsApp')
       redirectUri: 'http://localhost:9000/auth/soundcloud',
       clientId: '66fd2fb89c48bdcf4d67c15d99baf2a8',
       authorizationEndpoint: 'https://soundcloud.com/connect',
+      requiredUrlParams: ['scope'],
+      scope: 'non-expiring'
     });
   });
--- server/auth/soundcloud/index.js
+++ server/auth/soundcloud/index.js
@@ -2,11 +2,37 @@
 
 var express = require('express');
 var passport = require('passport');
+var request = require('request');
 
 var router = express.Router();
 
 router.post('/', function(req, res) {
-  
+  var accessTokenUrl = 'https://api.soundcloud.com/oauth2/token';
+  var soundcloudUserUrl = 'https://api.soundcloud.com/me.json'
+  var params = {
+    code: req.body.code,
+    client_id: req.body.clientId,
+    client_secret: 'e7871749b858f8d54e34a1f8f1984eba', //Should be hidden in config
+    grant_type: 'authorization_code',
+    redirect_uri: req.body.redirectUri
+  };
+
+  //Use the authorization code to get an access token
+  request.post(accessTokenUrl, {form: params, json: true}, function(err, response, body) {
+    if(!err) {
+      var accessToken = body.access_token;
+      var qs = {oauth_token: accessToken};
+      console.log(qs);
+
+      request.get({url: soundcloudUserUrl, qs: qs, json: true}, function(err, response, body) {
+        console.log(err);
+        console.log(body);
+      });
+    }
+  });
+
+
+
 });
 
 
