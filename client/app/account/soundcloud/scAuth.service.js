dbcb2c1c496cd9f6e6c945d314b0dfd99617a99f
zgleicher <zachgleicher@gmail.com>
2015-03-13 23:14:49 -0400

    feat(sc authentication) Build service to get favorite tracks of user. Get user id of logged in user

---
+++ client/app/account/soundcloud/scAuth.service.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/***************
+Service can be use for autenticationg. Acutal login, logout should be done with $auth service.
+Methods implemented to get the user id of the user logged in and the favorite tracks of a given user
+***************/
+
+angular.module('levelsApp')
+  .service('scAuthService', function ($auth, $http, $q) {
+
+    this.getUserId = function() {
+      var deferred = $q.defer();
+      if (!$auth.isAuthenticated()) { 
+        deferred.reject('Not Logged In');
+      }
+        var token = $auth.getToken();
+        $http({
+          url: 'https://api.soundcloud.com/me.json',
+          method: 'GET',
+          params: {oauth_token: token}
+        })
+        .then(function (response) {
+          var userId = response.data.id;
+          deferred.resolve(userId);
+        });
+        return deferred.promise;
+    }; 
+
+  // Gets the favorite tracks of a user given a user Id
+  this.getFavoriteTracks = function(userId) {
+     var deferred = $q.defer();
+        $http({
+          url: 'https://api.soundcloud.com/users/'+ userId + '/favorites', 
+          method: 'GET',
+          params: {client_id: '66fd2fb89c48bdcf4d67c15d99baf2a8'}
+        })
+        .then(function (response) {
+          deferred.resolve(response.data);
+        });
+
+        return deferred.promise;
+  }
+
+});
--- client/app/account/soundcloud/soundcloud.controller.js
+++ client/app/account/soundcloud/soundcloud.controller.js
@@ -1,7 +1,13 @@
 'use strict';
 
+/*****************
+// This controller demnstrates how the user information will be used. $auth functions 
+// should be attached to the appropriate controllers. 
+// Check out the api for Satellizer https://github.com/sahat/satellizer
+*****************/
+
 angular.module('levelsApp')
-  .controller('SoundcloudCtrl', function ($scope, $auth) {
+  .controller('SoundcloudCtrl', function ($scope, $auth, scAuthService) {
    $scope.authenticate = function(provider) {
       $auth.authenticate(provider).then(function(response) {
         //Login Success
@@ -18,10 +24,20 @@ angular.module('levelsApp')
 
   $scope.logout = function() {
     $auth.logout();
-  }
+  };
 
   $scope.getUserId = function() {
-    
-  }
+    scAuthService.getUserId().then(function(id) {
+      $scope.userId = id;
+    });
+  };
+
+  $scope.getFavoriteTracks = function() {
+    scAuthService.getUserId().then(function(id) {
+      scAuthService.getFavoriteTracks(id).then(function(tracks) {
+        $scope.tracks = tracks;
+      });
+    });
+  };
 
   });
