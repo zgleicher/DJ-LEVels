9c8bf9d892d4e43b83ee3e12ea12e086642795f8
Kristie Howard <kbh2120@columbia.edu>
2015-03-27 16:13:56 -0400

    fix(account.js): fixed hostname error for heroku

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -24,11 +24,13 @@ angular.module('levelsApp')
       //   controller: 'SettingsCtrl',
       //   authenticate: true
       // });
-
+    var host = location.protocol + '//' + location.host;
+    console.log('host is ' + host);
     $authProvider.oauth2({
       name: 'soundcloud',
       url: '/auth/soundcloud',
-      redirectUri: 'http://localhost:9000/auth/soundcloud',
+      // redirectUri: 'http://localhost:9000/auth/soundcloud',
+      redirectUri: host + '/auth/soundcloud',
       clientId: '66fd2fb89c48bdcf4d67c15d99baf2a8',
       authorizationEndpoint: 'https://soundcloud.com/connect',
       requiredUrlParams: ['scope'],
